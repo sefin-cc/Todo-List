a896354ac4b2c9876ef831db7bd6571c62ee3bcf
sefin-cc <rogenasefin6@gmail.com>
2024-12-11 00:32:41 +0800

    Delete, Edit, and  Local Storage

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 
 export default function TodoInput(props) {
-    const { handleAddTodos } = props
-    const [todoValue, setTodoValue] = useState('')
+    const { handleAddTodos , todoValue, setTodoValue} = props
+    // const [todoValue, setTodoValue] = useState('')
 
     return (
         <header>
